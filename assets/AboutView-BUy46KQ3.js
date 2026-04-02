6e916b189511e060ab77871de9e784b65afb2dde
Angelo Cruz <angelocruz.vgd@gmail.com>
2026-04-02 19:14:38 -0300

    Updates

--- assets/AboutView-BUy46KQ3.js
+++ assets/AboutView-BUy46KQ3.js
@@ -1,4 +1,4 @@
-import{c as n,u as d,j as e}from"./index-BMlv4zS-.js";import{M as c,L as x,G as m,B as h}from"./mail-Bgu58916.js";/**
+import{c as n,u as d,j as e}from"./index--7x0fZYB.js";import{M as c,L as x,G as m,B as h}from"./mail-B8Lv6pfE.js";/**
  * @license lucide-react v0.555.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/mail-B8Lv6pfE.js
+++ assets/mail-B8Lv6pfE.js
@@ -1,4 +1,4 @@
-import{c}from"./index-BMlv4zS-.js";/**
+import{c}from"./index--7x0fZYB.js";/**
  * @license lucide-react v0.555.0 - ISC
  *
  * This source code is licensed under the ISC license.
